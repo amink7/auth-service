fa3c1d62f4053e3f089d7d1fbae40b253dbb3eda
amink7 <amin_lan@hotmail.es>
2025-07-13 18:20:46 +0200

    Configurar tests con H2 usando perfil test

--- src/test/java/com/cybersecurity/auth_service/controller/LoginIntegrationTest.java
+++ src/test/java/com/cybersecurity/auth_service/controller/LoginIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.*;
@@ -17,6 +18,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
+
 @AutoConfigureMockMvc
 public class LoginIntegrationTest {
 
